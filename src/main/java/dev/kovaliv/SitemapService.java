b5c5384c2aa6615dd98bcbdf2c99387313a04950
Taras Kovaliv <taraskovaliv@outlook.com>
2024-09-22 14:09:39 +0300

    Add sitemap and robots.txt.

---
+++ src/main/java/dev/kovaliv/SitemapService.java
@@ -0,0 +1,20 @@
+package dev.kovaliv;
+
+import dev.kovaliv.services.sitemap.AbstractSitemapService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+import static cz.jiripinkas.jsitemapgenerator.ChangeFreq.WEEKLY;
+
+@Service
+public class SitemapService extends AbstractSitemapService {
+    @Override
+    protected Map<String, SMValue> getUrls() {
+        return Map.of(
+                "/", new SMValue(1.0, WEEKLY),
+                "/?lang=en", new SMValue(0.9, WEEKLY),
+                "/?lang=uk", new SMValue(0.9, WEEKLY)
+        );
+    }
+}

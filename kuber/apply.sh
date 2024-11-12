31d0cb1d67f72ba8bd6610ab5f0affa6593ff5ab
Taras Kovaliv <taraskovaliv@outlook.com>
2024-11-12 23:06:50 +0200

    Update kuber configs

--- kuber/apply.sh
+++ kuber/apply.sh
@@ -2,5 +2,5 @@ docker system prune -a -f
 git pull --rebase
 sudo docker compose build
 sudo docker image tag profile-api:latest registry.localhost/profile-api:latest
-sudo docker push --tls-verify=false registry.localhost/profile-api:latest
+sudo docker push registry.localhost/profile-api:latest
 sudo kubectl apply -f kuber/profile.yaml

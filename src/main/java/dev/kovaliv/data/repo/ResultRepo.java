1e45bbddc5814e345cdbf3d91f4cae4cfd53747a
Taras Kovaliv <taraskovaliv@outlook.com>
2025-03-18 18:22:07 +0200

    Fix treads endpoint.

--- src/main/java/dev/kovaliv/data/repo/ResultRepo.java
+++ src/main/java/dev/kovaliv/data/repo/ResultRepo.java
@@ -2,6 +2,8 @@ package dev.kovaliv.data.repo;
 
 import dev.kovaliv.data.entity.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ResultRepo extends JpaRepository<Result, Integer> {
 }
